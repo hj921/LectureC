d99d51d32dadbb6468536fcdfc8dbf2e79f176eb
hj921 <comdat88@gmail.com>
2018-10-20 18:32:55 +0900

    Data Type

---
+++ Lecture05/Lecture05/ex05_1.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main() {
+	/*
+	// int: 32비트(4바이트), 정수를 담는 데 쓰임
+	int a = 5;
+	int b = 3;
+
+	int hap = a + b;
+	int cha = a - b;
+	int gop = a * b;
+	int mok = a / b; // 5를 3으로 나눈 몫
+	int namuji = a % b;
+	// 5 % 3 = 2
+
+	printf("%d + %d = %d\n", a, b, hap);
+	printf("%d - %d = %d\n", a, b, cha);
+	printf("%d * %d = %d\n", a, b, gop);
+	printf("%d / %d = %d\n", a, b, mok);
+	printf("%d %% %d = %d\n", a, b, namuji);
+	*/
+
+	/*
+	// float: 32비트(4바이트), 실수를 담는 데 쓰임
+	float a = 9.8;
+	float b = 3.14;
+
+	float hap = a + b;
+	float cha = a - b;
+	float gop = a * b;
+	float mok = a / b; // 5를 3으로 나눈 몫
+	
+
+	printf("%f + %f = %f\n", a, b, hap);
+	printf("%f - %f = %f\n", a, b, cha);
+	printf("%f * %f = %f\n", a, b, gop);
+	printf("%f / %f = %f\n", a, b, mok);
+	*/
+
+	// double: 64비트(8바이트), 실수를 담는 데 쓰임
+	double a = 9.8;
+	double b = 3.14;
+
+	double hap = a + b;
+	double cha = a - b;
+	double gop = a * b;
+	double mok = a / b; // 5를 3으로 나눈 몫
+
+
+	printf("%f + %f = %f\n", a, b, hap);
+	printf("%f - %f = %f\n", a, b, cha);
+	printf("%f * %f = %f\n", a, b, gop);
+	printf("%f / %f = %f\n", a, b, mok);
+
+	// 부동 소주점(floating point)
+
+}
