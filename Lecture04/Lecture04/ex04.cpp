51e835b0ad96e2da35f08a93b238dae4d2044e83
hj921 <comdat88@gmail.com>
2018-10-20 17:55:44 +0900

    3rd days variable

---
+++ Lecture04/Lecture04/ex04.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int a; // 선언
+	a = 3; // 대입
+	printf("%d\n", a);
+
+	a = 5; // 대입
+	printf("%d\n", a);
+
+}
---
+++ Lecture04/Lecture04/ex04_2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int a = 2; // 초기화
+	int b = 3;
+
+	printf("%d + %d =  %d\n", a, b, a + b);
+
+	// 변수 이름 : 알파멧 대소문자, _(underscore), 숫자(첫번째 글자로는 사용불가)
+	int abc;
+	//int 3a; 불가
+	int a2;
+	int AAAA;
+	int unser_score;
+}
