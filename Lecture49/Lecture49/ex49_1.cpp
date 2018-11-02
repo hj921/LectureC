9a6840daec26e3505cb8a781dc0671ad27954b8f
hj921 <comdat88@gmail.com>
2018-11-02 22:04:43 +0900

    C++ I/O

---
+++ Lecture49/Lecture49/ex49_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // istream, ostream, iostream
+
+using namespace std;
+
+namespace b
+{
+	int n;
+}
+int main() {
+	cout << "Hello, World!" << endl;
+	//std::cout << "Hello, World!" << 'c' << 10 << std::endl;
+	
+}
---
+++ Lecture49/Lecture49/ex49_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "¿Ã∏ß ¿‘∑¬ : ";
+	cin >> name;
+
+	string message = "æ»≥Á«œººø‰, " + name + " ¥‘.";
+	cout << message << endl;
+	/*
+	string str;
+	str = "Hello";
+	cout << str << endl;
+	*/
+}
