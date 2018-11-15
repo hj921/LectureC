e4dc3657321ddd27740dc6d1925e8ea7081bef49
hj921 <comdat88@gmail.com>
2018-11-15 23:52:15 +0900

    const method

--- Lecture60/Lecture60/ex60_1.cpp
+++ Lecture60/Lecture60/ex60_1.cpp
@@ -7,20 +7,27 @@ public:
 	Account() : money(0) {		}
 	Account(int money) : money(money) {	}
 	
-	void Deposit(int d) {
+	void Deposit(const int d) {
+		// d = money; // 버그 생성
 		money += d;
 		cout << d << "원을 예금했다!!" << endl;
 	}
-	void Draw(int d) {
+	void Draw(const int d) {
 		if (money >= d) {
 			money -= d;
 			cout << d << "원을 인출했다!!" << endl;
 		}
 	}
 
-	int ViewMoney() {
+	int ViewMoney() const {
+		// money++; // 실수(버그) 생성, 이러한 실수 방지 -> 메서드 상수화
 		return money;
 	}
+	/* // ViewMoney의 리턴 값이 상수라는 의미. 상수형 메서드를 만드는 것은 아님
+	const int ViewMoney() {
+		return money;
+	}
+	*/
 private:
 	int money;
 };
